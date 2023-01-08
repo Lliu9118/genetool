e9890701e95131d24c8e153191a02ad2f65bbcbd
Lliu9118 <115491253+Lliu9118@users.noreply.github.com>
2023-01-08 13:00:01 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function setup() {
   
   
    linkWhiteBRCA.position(203, 434);
-  linkIndianBRCA = createA('https://docs.google.com/spreadsheets/d/1QUiOCaXBLiJsvYbiWbFjqV8tyL2GPLPjThW4Zoff0tw/edit#gid=0', 'this is a link','_blank');
+  linkIndianBRCA = createA('https://docs.google.com/spreadsheets/d/1QUiOCaXBLiJsvYbiWbFjqV8tyL2GPLPjThW4Zoff0tw/edit#gid=0', 'Click Here','_blank');
    linkIndianBRCA.position(203, 581);
   linkAsianLUAD = createA('https://docs.google.com/spreadsheets/d/1QUiOCaXBLiJsvYbiWbFjqV8tyL2GPLPjThW4Zoff0tw/edit#gid=0', 'this is a link','_blank');
    linkAsianLUAD.position(350, 287);
